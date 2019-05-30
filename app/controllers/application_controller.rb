e05fff73548a2a2982e3f7109fd775700fd9b042
Cai Yang <caih.yang@gmail.com>
2019-05-30 13:35:36 -0400

    auth,login, signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+
   def authorize_request
     header = request.headers['Authorization']
     header = header.split(' ').last if header
@@ -11,4 +12,5 @@ class ApplicationController < ActionController::API
       render json: { errors: e.message }, status: :unauthorized
     end
   end
+  
 end
